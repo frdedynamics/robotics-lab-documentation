6e8949a954ce12f8707881d3b13918bb9b866a9d
LauEls <44226646+LauEls@users.noreply.github.com>
2021-10-05 17:01:46 +0200

    Update quaternions.py

--- source/_static/scripts/ros_navigation/quaternions.py
+++ source/_static/scripts/ros_navigation/quaternions.py
@@ -1,6 +1,7 @@
 from geometry_msgs.msg import Quaternion
 
 def calcOrientation(theta):
+    v = (0,0,1)
     orientation = Quaternion()
     orientation.x = v[0]*math.sin(theta/2)
     orientation.y = v[1]*math.sin(theta/2)
