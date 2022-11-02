091d096167c7f493e33661b3facf18f3ec2937a2
LauEls <44226646+LauEls@users.noreply.github.com>
2022-11-02 10:32:10 +0100

    Update robot_dh.m

--- source/_static/scripts/build_custom_robot/robot_dh.m
+++ source/_static/scripts/build_custom_robot/robot_dh.m
@@ -1,7 +1,10 @@
 close all
 import ETS3.*
 
-j = Revolute('d', 0, 'a', 0, 'alpha', 0, 'offset', 0);
+j1 = Revolute('d', L2, 'a', 0, 'alpha', -pi/2, 'offset', 0);
+j2 = Revolute('d', 0, 'a', -L3, 'alpha', 0, 'offset', pi/2);
+j3 = Revolute('d', 0, 'a', -L4, 'alpha', 0, 'offset', 0);
+j4 = Revolute('d', 0, 'a', -L5, 'alpha', 0, 'offset', 0);
 
 robot = SerialLink([j1 j2 j3 j4],'name', 'my robot');
 
